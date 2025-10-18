737ae211e2ba118d4636c1ba95ec513d9c5418e6
nkhieu215 <102019148+nkhieu215@users.noreply.github.com>
2025-10-18 08:56:37 +0700

    Update Supply.java

--- BE/qltb/src/main/java/io/rd/qltb/domain/Supply.java
+++ BE/qltb/src/main/java/io/rd/qltb/domain/Supply.java
@@ -1,5 +1,6 @@
 package io.rd.qltb.domain;
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.FetchType;
@@ -22,6 +23,7 @@ import lombok.Setter;
 @Table(name = "Supplies")
 @Getter
 @Setter
+@JsonIgnoreProperties({"hibernateLazyInitializer", "handler"})
 public class Supply {
 
     @Id
