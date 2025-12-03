b66e541aa0d6a1caecc2345060be6d11f422c4da
QuangVinh <vinhls2002@gmail.com>
2025-12-03 16:42:04 +0700

    Update Approval.java

--- BE/qltb/src/main/java/io/rd/qltb/domain/Approval.java
+++ BE/qltb/src/main/java/io/rd/qltb/domain/Approval.java
@@ -36,7 +36,7 @@ public class Approval {
     @JoinColumn(name = "group_id", nullable = false)
     private ApprovalGroup group;
 @ManyToOne(fetch = FetchType.LAZY)
-    @JoinColumn(name = "round_id")
+    @JoinColumn(name = "round_id" , nullable = true)
     private ApprovalRound round;
     @Column
     private Integer status;
