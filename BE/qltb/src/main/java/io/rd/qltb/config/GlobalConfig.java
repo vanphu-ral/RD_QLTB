b5053c51e6cc12dbd9eb0834a0a2d98a73a21d68
nkhieu215 <102019148+nkhieu215@users.noreply.github.com>
2025-12-06 13:48:46 +0700

    Update GlobalConfig.java

--- BE/qltb/src/main/java/io/rd/qltb/config/GlobalConfig.java
+++ BE/qltb/src/main/java/io/rd/qltb/config/GlobalConfig.java
@@ -1,5 +1,26 @@
 package io.rd.qltb.config;
 
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.stereotype.Component;
+
+@Component
 public class GlobalConfig {
-    public static String  IP_ADDRESS_FRONTEND = "http://192.168.18.127:4200";
+    public static String  IP_ADDRESS_FRONTEND = "http://192.168.18.124:4200";
+
+    public String createNumberPrefix(Long number, int minLength) {
+        String numberStr = String.valueOf(number);
+        int numberLength = numberStr.length();
+
+        if (numberLength >= minLength) {
+            return numberStr; // nếu dài hơn hoặc bằng thì giữ nguyên
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < minLength - numberLength; i++) {
+            sb.append('0');
+        }
+        sb.append(numberStr);
+        return sb.toString();
+    }
 }
