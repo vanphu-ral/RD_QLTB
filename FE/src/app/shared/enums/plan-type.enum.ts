90931142c663a6fc0c54c4ec1c27c6a7243e41cc
QuangVinh <vinhls2002@gmail.com>
2025-12-15 15:25:20 +0700

    commit

--- FE/src/app/shared/enums/plan-type.enum.ts
+++ FE/src/app/shared/enums/plan-type.enum.ts
@@ -1,5 +1,5 @@
 export enum PLANTYPE {
-  AUDIT = 'AUDIT', //Kiểm tra
+  DAILYCHECK = 'DAILYCHECK', //Kiểm tra
   MAINTENANCE = 'MAINTENANCE', // Bảo trì
   REPAIR = 'REPAIR', //Sửa chữa
   OTHER = 'OTHER'
