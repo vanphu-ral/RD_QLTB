4d0586947e4da9d52354956ff61b882d913acd8b
QuangVinh <vinhls2002@gmail.com>
2026-01-16 17:19:23 +0700

    commit

--- FE/src/app/shared/pages/PlanManager/SampleReport/Detail/sample-report-detail.component.ts
+++ FE/src/app/shared/pages/PlanManager/SampleReport/Detail/sample-report-detail.component.ts
@@ -30,7 +30,8 @@ import { OperationsStaff, OperationsStaffLabel } from '../../../../enums/operati
 })
 export class SampleReportDetailComponent extends BasePageComponent<SampleReport> {
 
-  listFrequencies: any[] = ["Đầu giờ", "Cuối giờ", "Ngày", "Tuần", "Tháng", "Quỹ", "6 Tháng", "Năm"];
+  listFrequencies: any[] = ["Ngày", "Tuần", "Tháng", "Quỹ", "6 Tháng", "Năm"];
+  listFrequencieForCriterial: any[] = ["Đầu giờ", "Cuối giờ", "Ngày", "Tuần", "Tháng", "Quỹ", "6 Tháng", "Năm"];
   listTypes: any[] = [];
   listBranchs: any[] = [];
   listApprovalWorkflow: any[] = []
