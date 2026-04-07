816dc3fa6f3ca3d698264821f0a550f14690368e
nkhieu215 <102019148+nkhieu215@users.noreply.github.com>
2026-04-07 15:29:26 +0700

    Update plan.service.ts

--- FE/src/app/shared/pages/PlanManager/Plan/Service/plan.service.ts
+++ FE/src/app/shared/pages/PlanManager/Plan/Service/plan.service.ts
@@ -1,6 +1,9 @@
 import { Injectable } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { BaseApiService, CreateEntity } from '../../../../service/base-api.service';
+import {
+  BaseApiService,
+  CreateEntity,
+} from '../../../../service/base-api.service';
 import { Plan } from '../../../../models/PlanManger/plan.model';
 import { Observable } from 'rxjs';
 import { PlanRequest } from '../../../../models/PlanManger/plan-request.model';
@@ -10,8 +13,8 @@ export interface Page<T> {
   content: T[];
   totalElements: number;
   totalPages: number;
-  number: number;   // số trang hiện tại (0-based)
-  size: number;     // số bản ghi mỗi trang
+  number: number; // số trang hiện tại (0-based)
+  size: number; // số bản ghi mỗi trang
   first: boolean;
   last: boolean;
   numberOfElements: number;
@@ -25,8 +28,7 @@ export class PlanService extends BaseApiService<PlanRequest> {
   }
 
   getPlans(page: number, size: number, filters: any): Observable<any> {
-    let params = new HttpParams()
-      .set('page', page)
+    let params = new HttpParams().set('page', page);
 
     Object.keys(filters).forEach((key) => {
       const value = filters[key];
@@ -38,8 +40,10 @@ export class PlanService extends BaseApiService<PlanRequest> {
     return this.http.get<any>(`${this['fullBaseUrl']}/paged`, { params });
   }
 
-  createPlanWithDetails(entity: CreateEntity<PlanRequest>): Observable<PlanRequest> {
-    let newObj = _.omit(entity, ["status", "createdBy", "updatedBy"]);
+  createPlanWithDetails(
+    entity: CreateEntity<PlanRequest>,
+  ): Observable<PlanRequest> {
+    let newObj = _.omit(entity, ['status', 'createdBy', 'updatedBy']);
     return this.http.post<PlanRequest>(`${this['fullBaseUrl']}/create`, newObj);
   }
 
@@ -58,35 +62,70 @@ export class PlanService extends BaseApiService<PlanRequest> {
   getScheduleMaintance(id: number | string): Observable<any> {
     return this.http.get<any>(`${this['fullBaseUrl']}/details/${id}`);
   }
-
+  getScheduleMaintanceNew(id: number | string): Observable<any> {
+    return this.http.get<any>(`${this['fullBaseUrl']}/details/maintain/${id}`);
+  }
   updateStatus(id: number, value: number) {
-    return this.http.put<void>(`${this['fullBaseUrl']}/${id}/status?value=${value}`, {});
+    return this.http.put<void>(
+      `${this['fullBaseUrl']}/${id}/status?value=${value}`,
+      {},
+    );
   }
 
   override create(entity: CreateEntity<PlanRequest>): Observable<PlanRequest> {
     const now = new Date();
-    const isoLocalVN = now.getFullYear() + '-' +
-      String(now.getMonth() + 1).padStart(2, '0') + '-' +
-      String(now.getDate()).padStart(2, '0') + 'T' +
-      String(now.getHours()).padStart(2, '0') + ':' +
-      String(now.getMinutes()).padStart(2, '0') + ':' +
+    const isoLocalVN =
+      now.getFullYear() +
+      '-' +
+      String(now.getMonth() + 1).padStart(2, '0') +
+      '-' +
+      String(now.getDate()).padStart(2, '0') +
+      'T' +
+      String(now.getHours()).padStart(2, '0') +
+      ':' +
+      String(now.getMinutes()).padStart(2, '0') +
+      ':' +
       String(now.getSeconds()).padStart(2, '0');
-    const newEntity = { ...entity, plan: { ...entity.plan, createdAt: isoLocalVN, updatedAt: isoLocalVN, createdBy: this.accountService.getUser()?.fullName ?? 'unknown' } };
+    const newEntity = {
+      ...entity,
+      plan: {
+        ...entity.plan,
+        createdAt: isoLocalVN,
+        updatedAt: isoLocalVN,
+        createdBy: this.accountService.getUser()?.fullName ?? 'unknown',
+      },
+    };
     return this.http.post<PlanRequest>(this['fullBaseUrl'], newEntity);
   }
 
-  override update(id: number | string, data: PlanRequest): Observable<PlanRequest> {
+  override update(
+    id: number | string,
+    data: PlanRequest,
+  ): Observable<PlanRequest> {
     const now = new Date();
-    const updatedAtVN = now.getFullYear() + '-' +
-                    String(now.getMonth() + 1).padStart(2, '0') + '-' +
-                    String(now.getDate()).padStart(2, '0') + 'T' +
-                    String(now.getHours()).padStart(2, '0') + ':' +
-                    String(now.getMinutes()).padStart(2, '0') + ':' +
-                    String(now.getSeconds()).padStart(2, '0');
+    const updatedAtVN =
+      now.getFullYear() +
+      '-' +
+      String(now.getMonth() + 1).padStart(2, '0') +
+      '-' +
+      String(now.getDate()).padStart(2, '0') +
+      'T' +
+      String(now.getHours()).padStart(2, '0') +
+      ':' +
+      String(now.getMinutes()).padStart(2, '0') +
+      ':' +
+      String(now.getSeconds()).padStart(2, '0');
     const updatedEntity = {
       ...data,
-      plan: { ...data.plan, updatedAt: updatedAtVN, updatedBy: this.accountService.getUser()?.fullName ?? 'unknown' },
+      plan: {
+        ...data.plan,
+        updatedAt: updatedAtVN,
+        updatedBy: this.accountService.getUser()?.fullName ?? 'unknown',
+      },
     };
-    return this.http.put<PlanRequest>(`${this['fullBaseUrl']}/${id}`, updatedEntity);
+    return this.http.put<PlanRequest>(
+      `${this['fullBaseUrl']}/${id}`,
+      updatedEntity,
+    );
   }
 }
